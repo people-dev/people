4d77756ea63aeb075afce5de073b268a3c153942
Julian Deinert <j.deinert@gmx.net>
2015-05-12 23:21:51 +0200

    Rework design for registration form

--- people/views.py
+++ people/views.py
@@ -19,5 +19,6 @@ class RegisterForm(Form):
 	"""docstring for RegisterForm"""
 	firstName = StringField('First Name', validators=[DataRequired()])
 	lastName = StringField('Last Name', validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 		
