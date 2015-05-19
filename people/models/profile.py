223c0b76aa8c2ecb32d8bc459b019b68abf5d3f9
Julian Deinert <j.deinert@gmx.net>
2015-05-19 09:02:52 +0200

    Add image to profile

--- people/models/profile.py
+++ people/models/profile.py
@@ -4,12 +4,14 @@ class Profile(db.Model):
     userid = db.Column(db.Text, primary_key=True)
     aboutText = db.Column(db.Text)
     gender = db.Column(db.Text)
+    image = db.Column(db.Text)
 
 
 
 
-    def __init__(self, userid, aboutText, gender):
+    def __init__(self, userid, aboutText, gender, image):
         self.userid = userid
         self.aboutText = aboutText
         self.gender = gender
+        self.image = image
 
