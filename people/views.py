3600ae44c90e9df376d4981c81451208df6389f8
Julian Deinert <j.deinert@gmx.net>
2015-05-21 22:16:02 +0200

    Add notification boxes to layout template and generate notifications for registration and login

--- people/views.py
+++ people/views.py
@@ -3,7 +3,8 @@ from flask import render_template, request, redirect, url_for, abort, flash
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
-from people.models.user import User
+from people.models import User
+from people.models import Profile
 from flask.ext.login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash
 
@@ -17,9 +18,15 @@ def register():
     form = RegisterForm()
     if request.method == 'POST' and form.validate():
         user = User(form.username.data, form.firstName.data, form.lastName.data, generate_password_hash(form.password.data))
+        if user.id is False:
+            flash('Username not valid', 'error')
+            abort(redirect('register'))
+        profile = Profile(form.username.data)
         db.session.add(user)
+        db.session.add(profile)
         db.session.commit()
-        return render_template('index.html')
+        flash("You've successfully registered. Now login with your credentials.", 'success')
+        return render_template('login.html')
     else:
         return render_template('register.html', form=form)
 
@@ -35,17 +42,20 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.get(form.username.data)
+        if user is None:
+            flash('unknown User', 'error')
+            abort(redirect('login'))
         if user.check_password(form.password.data):
-            login = login_user(user)
-            print(login)
-
-            flash('Logged in successfully.')
+            login_user(user)
+            flash('Logged in successfully.', 'success')
 
         # next = request.args.get('next')
         # if not next_is_valid(next):
         #     return abort(400)
 
             return redirect(url_for('index'))
+        else:
+            flash('Username or password wrong', 'error')
     return render_template('login.html', form=form)
 
 @app.route("/logout")
@@ -53,6 +63,15 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/<username>')
+@login_required
+def profile(username):
+    profile = Profile.query.get(username)
+    user = User.query.get(username)
+    if user is None:
+        abort(404)
+    return render_template('profile.html', profile=profile, user=user)
 
 
 class RegisterForm(Form):
