60d9a8f3dbdace3e8b686630e0d3665c3eb0073a
Julian Deinert <j.deinert@gmx.net>
2015-05-27 20:29:17 +0200

    Change layout of profilepage

--- people/models/profile.py
+++ people/models/profile.py
@@ -5,8 +5,15 @@ class Profile(db.Model):
     about = db.Column(db.Text)
     gender = db.Column(db.Text)
     image = db.Column(db.Text)
-    mayor = db.Column(db.Text)
+    major = db.Column(db.Text)
     semester = db.Column(db.Text)
+    email = db.Column(db.Text)
+    phone = db.Column(db.Text)
+    mobile = db.Column(db.Text)
+    jabber = db.Column(db.Text)
+    street = db.Column(db.Text)
+    zipcode = db.Column(db.Text)
+    city = db.Column(db.Text)
     updated_at = db.Column(db.Integer)
 
 
