a070566b5ce8337da54c3375d67f5bb7f3d2abb2
Julian Deinert <j.deinert@gmx.net>
2015-05-27 22:26:40 +0200

    Change profile model and use user-email on profilepage

--- people/models/profile.py
+++ people/models/profile.py
@@ -7,21 +7,28 @@ class Profile(db.Model):
     image = db.Column(db.Text)
     major = db.Column(db.Text)
     semester = db.Column(db.Text)
-    email = db.Column(db.Text)
     phone = db.Column(db.Text)
     mobile = db.Column(db.Text)
     jabber = db.Column(db.Text)
     street = db.Column(db.Text)
     zipcode = db.Column(db.Text)
     city = db.Column(db.Text)
-    updated_at = db.Column(db.Integer)
+    updated_at = db.Column(db.Integer, nullable=True)
 
 
 
 
-    def __init__(self, userid, about="", gender="", image=""):
+    def __init__(self, userid, about="", gender="", image="", major="", semester="", phone="", mobile="", jabber="", street="", zipcode="", city=""):
         self.userid = userid
         self.about = about
         self.gender = gender
         self.image = image
+        self.major = major
+        self.semester = semester
+        self.phone = phone
+        self.mobile = mobile
+        self.jabber = jabber
+        self.street = street
+        self.zipcode = zipcode
+        self.city = city
 
